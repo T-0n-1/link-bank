9a22bc27d8ad2301b0d9060135b083670fac7f56
Toni Mertanen <t.mertanen@hotmail.com>
2024-11-24 04:50:26 +0200

    BUGFIX Restructure bs-config.js because updated browser-sync

--- bs-config.js
+++ bs-config.js
@@ -1,16 +1,16 @@
-try {
-  require('dotenv').config();
-  console.log("Loaded environment variables from .env file");
-} catch (error) {
-  console.log("dotenv not installed or .env file not found; skipping .env configuration");
-}
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-require-imports */
+const dotenv = require('dotenv');
+dotenv.config();
 
-const serverPort = process.env.PORT || 3000;
+const serverPort = process.env.PROXYPORT || 3000;
+const host = process.env.SERVERNAME || 'localhost';
 
 module.exports = {
-  proxy: `http://localhost:`, // Proxy your Express server
+  proxy: `http://${host}:${serverPort}`, // Proxy your Express server
+  host: host, // Set the host option
   port: 4000, // BrowserSync’s port
-  files: ["src/**/*"], // Watch files for changes
+  files: ['src/**/*'], // Watch files for changes
   injectChanges: true, // Enable injecting changes
   reloadOnRestart: true, // Reload the browser when BS restarts
   reloadDebounce: 700,
